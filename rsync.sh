b7d05b03d1eb25c8f48e3ce2f0f2864a8e53172d
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-06-17 11:27:40 +0800

    Add files via upload

---
+++ rsync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "RSync Remote Copy Script"
+echo "This script will generate the command to copy a file or folder remotely"
+read -p "Enter the SSH Port Number: " port
+read -p "Enter the Remote Server IP: " ip
+read -p "Enter the Remote UserName: " uname
+read -p "Enter the File or Folder Name: " file
+printf "\n"
+echo "RSync Command Details:"
+echo "FILE COPY:"
+echo "rsync -av -e 'ssh -p $port ' --progress $file $uname@$ip: "
+echo "FOLDER COPY:"
+echo "rsync -avr -e 'ssh -p $port ' --progress $file/ $uname@$ip: "
